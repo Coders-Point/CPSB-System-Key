e706c1d0383fda6bda2fee6526d34626d4115995
Coders-Point <coderspoint7@gmail.com>
2025-02-03 23:37:14 +0530

    Update app.py
    Changed configurations in app.run()

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import json
+import os
 
 
 app = Flask(__name__)
@@ -104,4 +105,4 @@ def delete_bank():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
